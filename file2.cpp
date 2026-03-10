6d0a0f821f24439c4a64868bb678405ca40c86a1
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-03-10 19:21:08 +0700

    menambahkan proses memasukkan nilai bi dan mt kemudian menampilkan hasilnya

--- file2.cpp
+++ file2.cpp
@@ -19,4 +19,19 @@ string Status_lulus(float r){
 int main(){
     float nilBI, nilMT, rerata;
     string status;
+
+    cout <<"Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout <<"Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    //rerata = (nilBI + nillMT)/2;
+    //if (rerata >= 60)
+    //  status = "Lolos";
+    //else
+    //  status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
+
+    cout << "Status kelulusan = " << Status_lulus(rerata) << " ,dengan nilai rerata = " <<rerata<< endl;
 }
